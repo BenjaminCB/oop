fd543dc3ebda2561c858b4ed1f2868cd01b33966
BenjaminCB <benjamincb@hotmail.dk>
2021-09-17 12:05:55 +0200

    simple main functino to test person filter

--- 2_lecture/exercises/Program.cs
+++ 2_lecture/exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exercises
 {
@@ -6,7 +7,32 @@ namespace exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> PS = new List<Person>();
+            PS.Add(new Person("Benjamin", "Bennetzen", 21));
+            PS.Add(new Person("Peter", "Bennetzen", 21));
+            PS.Add(new Person("Tor", "Bennetzen", 21));
+            PS.Add(new Person("Gonde", "Bennetzen", 21));
+            PS.Add(new Person("Daniel", "Bennetzen", 21));
+            PS.Add(new Person("Nikolaj", "Bennetzen", 21));
+            PS.Add(new Person("Flemming", "Bennetzen", 21));
+
+            PersonFilter OnName = new NameFilter("Benjamin");
+            PersonFilter NotOnName = new NotFilter(OnName);
+
+            List<Person> OnNamePS = OnName.Filter(PS);
+            List<Person> NotOnNamePS = NotOnName.Filter(PS);
+
+            foreach (Person p in OnNamePS)
+            {
+                Console.WriteLine(p.FirstName);
+            }
+
+            Console.WriteLine("");
+
+            foreach (Person p in NotOnNamePS)
+            {
+                Console.WriteLine(p.FirstName);
+            }
         }
     }
 }
