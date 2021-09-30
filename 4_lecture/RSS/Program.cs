042d8aee61290656823078ed2782592fdc9b349a
BenjaminCB <benjamincb@hotmail.dk>
2021-09-30 19:03:45 +0200

    small testing with rss feeds

---
+++ 4_lecture/RSS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+using System;
+
+namespace RSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.dr.dk/nyheder/service/feeds/allenyheder";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                Console.WriteLine(item.Title.Text);
+                // Something is wrong with drs Summary.Text i think
+                /* Console.WriteLine(item.Summary.Text); */
+            }
+        }
+    }
+}
