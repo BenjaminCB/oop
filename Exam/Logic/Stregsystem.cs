e8b42420f0c20c33e990db4f5dd4aaf929bc6e21
BenjaminCB <benjamincb@hotmail.dk>
2021-11-13 18:01:55 +0100

    almost implement IStregsystem interface

--- Exam/Logic/Stregsystem.cs
+++ Exam/Logic/Stregsystem.cs
@@ -1,34 +1,42 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using Exam.Exception;
 
 namespace Exam.Logic
 {
-    public class StregsystemController
+    public class StregsystemController : IStregsystem
     {
         public List<User> Users { get; }
         public List<Product> Products { get; }
         public List<Transaction> Transactions { get; }
+        private string _LogFile;
 
         public StregsystemController()
         {
             Users = new List<User>();
             Products = new List<Product>();
             Transactions = new List<Transaction>();
+            _LogFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "TransactionLog");
         }
 
-        public void BuyProduct(User user, Product product) =>
-            ExecuteTransaction(new BuyTransaction(user, product));
+        public BuyTransaction BuyProduct(User user, Product product) =>
+            new BuyTransaction(user, product);
 
-        public void AddCreditsToAccount(User user, int amount) =>
-            ExecuteTransaction(new InsertCashTransaction(user, amount));
+        public InsertCashTransaction AddCreditsToAccount(User user, int amount) =>
+            new InsertCashTransaction(user, amount);
 
         // TODO find out if i want try catch here or later
         public void ExecuteTransaction(Transaction t)
         {
             t.Execute();
             Transactions.Add(t);
+
+            // TODO check if this writes everything and overwrites on restart
+            // Probably should do this async
+            using StreamWriter file = new StreamWriter(_LogFile);
+            file.WriteLine(t);
         }
 
         // TODO refactor GetProductById and GetUserByEsername
@@ -40,10 +48,9 @@ namespace Exam.Logic
             return Products.Find(p => p.Id == id);
         }
 
-        public List<User> GetUsers(Func<User,bool> predicate) =>
-            (List<User>) Users.Where(predicate);
+        public IEnumerable<User> GetUsers(Func<User,bool> predicate) => Users.Where(predicate);
 
-        public User GetUserByEsername(string username)
+        public User GetUserByUsername(string username)
         {
             if (!Users.Exists(u => u.Username == username))
                 throw new UsernameNotFoundException(username);
@@ -51,12 +58,12 @@ namespace Exam.Logic
             return Users.Find(u => u.Username == username);
         }
 
-        public List<Transaction> GetTransactions(User user, int n) =>
-            (List<Transaction>) Transactions.Where(t => t.User == user).Reverse().Take(n);
+        public IEnumerable<Transaction> GetTransactions(User user, int n) =>
+            Transactions.Where(t => t.User == user).Reverse().Take(n);
 
-        public List<Product> ActiveProducts
+        public IEnumerable<Product> ActiveProducts
         {
-            get => (List<Product>) Products.Where(p => p.Active);
+            get => Products.Where(p => p.Active);
         }
     }
 }
